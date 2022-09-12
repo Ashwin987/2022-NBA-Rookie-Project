9de4a7267e1298b43bc73eb084ef580a6271135b
Ashwin987 <ashwin.ramases@gmail.com>
2022-09-12 08:39:32 -0500

    first commit

---
+++ Recent Statistical trends full RCODE.R
@@ -0,0 +1,20 @@
+
+
+library(ggplot2)
+library(reshape2)
+
+data14
+
+boxplot(data14)
+
+
+boxplot(data14,  pch = 19, col="aquamarine3",
+        main="Statistical Trend (points per 40 mins) 2016-2022",
+        xlab = "Draft Year", 
+        ylab = "Points per 40 minutes")
+
+
+
+
+
+
